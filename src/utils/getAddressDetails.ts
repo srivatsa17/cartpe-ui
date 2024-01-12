7b3f2a1e601a296d535308b8d0e7959e786dc47f
Srivatsa J <srivatsa1199@gmail.com>
2024-01-12 20:00:14 +0530

    Add helper file for address component

---
+++ src/utils/getAddressDetails.ts
@@ -0,0 +1,14 @@
+import { ShippingAddress } from "utils/types";
+
+export const getDefaultAddress = (addressList: Array<ShippingAddress>) => {
+    const isDefaultAddressFound = addressList.find((address) => address.is_default);
+    const defaultAddress =
+        isDefaultAddressFound || (addressList.length > 0 ? addressList[0] : null);
+    return defaultAddress ? JSON.stringify(defaultAddress) : JSON.stringify("");
+};
+
+export const accordianStageKeys = {
+    SHIPPING_ADDRESS: "1",
+    ORDER_SUMMARY: "2",
+    PAYMENT_OPTIONS: "3"
+};
