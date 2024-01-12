c13986474da30b1c007672820c6e8abef4b51905
Srivatsa J <srivatsa1199@gmail.com>
2024-01-12 19:53:51 +0530

    Update price details file

--- src/utils/getCartPriceDetails.ts
+++ src/utils/getCartPriceDetails.ts
@@ -1,40 +1,45 @@
 import { Cart } from "./types";
+import { useReduxSelector } from "hooks/redux";
 
-export const getCartPriceDetails = (cartItems: Array<Cart>) => {
+export const getCartPriceDetails = () => {
+    const { cartItems } = useReduxSelector((state) => state.cart);
+
     const totalMRP = Number(
-        cartItems
-            .reduce(
-                (sum: number, cartItem: Cart) => sum + cartItem.quantity * cartItem.product.price,
-                0
-            )
-            .toFixed(2)
+        cartItems.reduce(
+            (sum: number, cartItem: Cart) => sum + cartItem.quantity * cartItem.product.price,
+            0
+        )
     );
     const totalDiscountPrice = Number(
-        cartItems
-            .reduce(
-                (sum: number, cartItem: Cart) =>
-                    sum + cartItem.quantity * cartItem.product.discounted_price,
-                0
-            )
-            .toFixed(2)
+        cartItems.reduce(
+            (sum: number, cartItem: Cart) =>
+                sum + cartItem.quantity * cartItem.product.discounted_price,
+            0
+        )
     );
     const totalSellingPrice = Number(
-        cartItems
-            .reduce(
-                (sum: number, cartItem: Cart) =>
-                    sum + cartItem.quantity * cartItem.product.selling_price,
-                0
-            )
-            .toFixed(2)
+        cartItems.reduce(
+            (sum: number, cartItem: Cart) =>
+                sum + cartItem.quantity * cartItem.product.selling_price,
+            0
+        )
     );
     const convenienceFee = 10;
+    const shippingFee = "FREE";
     const totalAmount = Math.round(totalSellingPrice + convenienceFee);
+    const roundOffPrice = Math.round(totalSellingPrice) - totalSellingPrice;
+    const savingsAmount = Math.round(totalMRP - totalAmount);
+    const savingsPercent = (savingsAmount / totalMRP) * 100;
 
     return {
         totalMRP,
         totalDiscountPrice,
         totalSellingPrice,
         convenienceFee,
-        totalAmount
+        shippingFee,
+        totalAmount,
+        roundOffPrice,
+        savingsAmount,
+        savingsPercent
     };
 };
