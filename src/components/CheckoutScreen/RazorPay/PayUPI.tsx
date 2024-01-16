cfa868ccffb5f800210201bff425b9e284535393
Srivatsa J <srivatsa1199@gmail.com>
2024-01-16 20:46:05 +0530

    Add pay by upi component

---
+++ src/components/CheckoutScreen/RazorPay/PayUPI.tsx
@@ -0,0 +1,151 @@
+/* eslint-disable camelcase */
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import {
+    ORDER_CONFIRMED_SCREEN,
+    ORDER_FAILED_SCREEN,
+    ORDER_PAYMENT_FAILED_SCREEN
+} from "constants/routes";
+import {
+    Order,
+    PaymentMethods,
+    RazorPayFailureHandlerArgs,
+    RazorpaySuccessHandlerArgs,
+    RazorpaySuccessResponse
+} from "utils/types";
+import useRazorpay, { Razorpay } from "hooks/useRazorpay";
+
+import { CARTPE_ICON_BLACK } from "constants/images";
+import { RAZORPAY_API_TEST_KEY } from "constants/razorpay";
+import { RAZORPAY_ORDER_URI } from "constants/api";
+import React from "react";
+import { axiosInstance } from "utils/axios";
+import { createOrder } from "hooks/useCreateOrder";
+import { useReduxSelector } from "hooks/redux";
+
+interface OrderDetails {
+    shippingAddressId: bigint | null;
+    orderItems: Array<{
+        product: bigint;
+        quantity: number;
+    }>;
+    amount: number;
+}
+
+interface UserDetails {
+    firstName?: string | null;
+    lastName?: string | null;
+    email?: string | null;
+}
+
+interface RazorpayDetails {
+    Razorpay: typeof Razorpay;
+}
+
+const DisplayRazorPayCheckoutForm = ({
+    Razorpay,
+    shippingAddressId,
+    orderItems,
+    amount,
+    firstName,
+    lastName,
+    email,
+    navigate
+}: OrderDetails & UserDetails & RazorpayDetails & { navigate: NavigateFunction }) => {
+    const razorpayOrderData = { amount: amount };
+
+    // Create the Razorpay order first by passing the amount.
+    // Once it succeeds, pass razorpay order and payment details to backend API to create order internally.
+    axiosInstance
+        .post(RAZORPAY_ORDER_URI, razorpayOrderData)
+        .then((response: RazorpaySuccessResponse) => {
+            const razorpayOrder = response.data;
+            const options = {
+                key: RAZORPAY_API_TEST_KEY,
+                amount: amount * 100,
+                currency: "INR",
+                name: "CartPe",
+                image: CARTPE_ICON_BLACK,
+                order_id: razorpayOrder.id,
+                handler: function (response: RazorpaySuccessHandlerArgs) {
+                    if (shippingAddressId && orderItems && amount > 0) {
+                        const createOrderProps = {
+                            method: "UPI" as PaymentMethods,
+                            razorpayOrderDetails: response,
+                            shippingAddressId,
+                            orderItems,
+                            amount
+                        };
+                        // Create an order at the backend and navigate to order confirmed screen.
+                        createOrder(createOrderProps).then((order: Order) => {
+                            navigate(
+                                `${ORDER_CONFIRMED_SCREEN}?paymentMethod=${order.method}&orderId=${order.id}&razorpayOrderId=${razorpayOrder.id}`,
+                                {
+                                    state: {
+                                        paymentMethod: order.method,
+                                        orderId: order.id,
+                                        razorpayOrderId: razorpayOrder.id
+                                    }
+                                }
+                            );
+                        });
+                    }
+                },
+                prefill: {
+                    name: firstName && lastName ? firstName + lastName : "",
+                    email: email ? email : "youremail@example.com"
+                    // Add phone number field once its implemented from backend
+                    // contact: "9999999999"
+                },
+                theme: {
+                    color: "#004493"
+                }
+            };
+
+            const razorpayInstance = new Razorpay(options);
+
+            // On payment fail, display payment failed screen.
+            razorpayInstance.on("payment.failed", (response: RazorPayFailureHandlerArgs) => {
+                const orderIdQueryParam = `razorpayOrderId=${response?.error?.metadata?.order_id}`;
+                navigate(`${ORDER_PAYMENT_FAILED_SCREEN}?${orderIdQueryParam}`, {
+                    state: {
+                        paymentErrorDescription: response?.error?.description
+                    }
+                });
+            });
+
+            razorpayInstance.open();
+        })
+        .catch(() => {
+            // Navigate to Order Failed Screen if razorpay order fails.
+            navigate(`${ORDER_FAILED_SCREEN}`);
+        });
+};
+
+function PayUPI() {
+    // Passing hooks from default component.
+    const [Razorpay] = useRazorpay();
+    const navigate = useNavigate();
+    const { shippingAddressId, orderItems, amount } = useReduxSelector(
+        (state) => state.checkoutDetails
+    );
+    const { firstName, lastName, email } = useReduxSelector((state) => state.userLoginDetails);
+
+    const checkoutFormProps = {
+        Razorpay,
+        navigate,
+        shippingAddressId,
+        orderItems,
+        amount,
+        firstName,
+        lastName,
+        email
+    };
+
+    React.useEffect(() => {
+        DisplayRazorPayCheckoutForm(checkoutFormProps);
+    }, [Razorpay]);
+
+    return null;
+}
+
+export default PayUPI;
