d254ec9e6dfa82b678d0e9aa49373497d3f598db
Srivatsa J <srivatsa1199@gmail.com>
2024-02-05 19:10:41 +0530

    Add sort by value also in filters applied

--- src/components/CategorySearchScreen/Filters/FilterAppliedChips.tsx
+++ src/components/CategorySearchScreen/Filters/FilterAppliedChips.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useFilterSearchParams } from "hooks/useFilterSearchParams";
 
 function FilterAppliedChips() {
-    const { filteredCategories, filteredBrands, filteredDiscount, filteredPriceRange } =
+    const { filteredCategories, filteredBrands, filteredDiscount, filteredPriceRange, sortBy } =
         useFilterSearchParams();
 
     const filtersApplied = [];
@@ -15,12 +15,15 @@ function FilterAppliedChips() {
     if (filteredPriceRange) {
         filtersApplied.push(`₹${filteredPriceRange[0]} - ₹${filteredPriceRange[1]}`);
     }
+    if(sortBy) {
+        filtersApplied.push(sortBy);
+    }
 
     return (
         <div className="w-full xs:grid grid-flow-row grid-cols-2 overflow-auto my-3">
             {filtersApplied.map((filter, index) => {
                 return (
-                    <Chip key={index} className="my-2 mr-2" color="secondary">
+                    <Chip key={index} className="my-2 mr-2 capitalize" color="secondary">
                         {filter}
                     </Chip>
                 );
