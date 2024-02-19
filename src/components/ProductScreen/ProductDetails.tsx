74367c9c5b4d437c867baf7bec4129ade29e4d80
Srivatsa J <srivatsa1199@gmail.com>
2024-02-20 01:05:51 +0530

    Fix product screen components

--- src/components/ProductScreen/ProductDetails.tsx
+++ src/components/ProductScreen/ProductDetails.tsx
@@ -1,148 +1,223 @@
-import { Button, Divider, Spacer } from "@nextui-org/react";
-import { useReduxDispatch, useReduxSelector } from "hooks/redux";
+import { Button, Image, Spacer } from "@nextui-org/react";
+// import { CashIcon } from "icons/CashIcon";
+// import { ExchangeIcon } from "icons/ExchangeIcon";
+// import { HeartFillIcon } from "icons/HeartFillIcon";
+import { Product, ProductVariant } from "utils/types";
 
-import { CashIcon } from "icons/CashIcon";
-import { ExchangeIcon } from "icons/ExchangeIcon";
-import { Product } from "utils/types";
+import { CartIcon } from "icons/CartIcon";
+import { HeartIcon } from "icons/HeartIcon";
+import { PLACEHOLDER_IMAGE } from "constants/images";
 import Rating from "components/CategorySearchScreen/Rating";
 import React from "react";
 import { RupeeIcon } from "icons/RupeeIcon";
-import { TruckFastIcon } from "icons/TruckFastIcon";
-import { TruckIcon } from "icons/TruckIcon";
-import { addCartItem } from "redux/CartService/cartSlice";
-import { addProductToWishList } from "redux/ProductService/wishlistSlice";
+import { getProductVariantProperties } from "utils/getProductVariantProperties";
+
+// import { useReduxDispatch, useReduxSelector } from "hooks/redux";
+
+// import { TruckFastIcon } from "icons/TruckFastIcon";
+// import { TruckIcon } from "icons/TruckIcon";
+// import { addCartItem } from "redux/CartService/cartSlice";
+// import { addProductToWishList } from "redux/ProductService/wishlistSlice";
 
 interface ProductDetailsProps {
-    product: Partial<Product>;
+    product: Product;
+    selectedProductVariant: ProductVariant;
+    setSelectedProductVariant: React.Dispatch<React.SetStateAction<ProductVariant>>;
 }
 
-function getDeliveryDate() {
-    const today = new Date();
-    today.setDate(today.getDate() + 3);
-    const formattedDate = today.toLocaleDateString("en-IN", {
-        weekday: "short",
-        month: "short",
-        day: "numeric"
-    });
-    return formattedDate;
-}
+// function getDeliveryDate() {
+//     const today = new Date();
+//     today.setDate(today.getDate() + 3);
+//     const formattedDate = today.toLocaleDateString("en-IN", {
+//         weekday: "short",
+//         month: "short",
+//         day: "numeric"
+//     });
+//     return formattedDate;
+// }
 
-function ProductDetails({ product }: ProductDetailsProps) {
-    const dispatch = useReduxDispatch();
-    const { cartItems } = useReduxSelector((state) => state.cart);
-    const { wishListedProducts } = useReduxSelector((state) => state.wishlist);
+function ProductDetails({
+    product,
+    selectedProductVariant,
+    setSelectedProductVariant
+}: ProductDetailsProps) {
+    // const dispatch = useReduxDispatch();
+    // const { cartItems } = useReduxSelector((state) => state.cart);
+    // const { wishListedProducts } = useReduxSelector((state) => state.wishlist);
 
-    const isProductInCart = cartItems.some((cartItem) => cartItem.product.id === product.id);
-    const isProductInWishList = wishListedProducts.some(
-        (wishListedProduct) => wishListedProduct.product.id === product.id
+    // const isProductInCart = cartItems.some((cartItem) => cartItem.product.id === product.id);
+    // const isProductInWishList = wishListedProducts.some(
+    //     (wishListedProduct) => wishListedProduct.product.id === product.id
+    // );
+
+    // const handleAddToCart = () => {
+    //     // Handle already exists condition as well with alert message.
+    //     const productWithStrictType = product as Product;
+    //     dispatch(addCartItem(productWithStrictType, 1));
+    // };
+
+    // const handleAddToWishList = () => {
+    //     if (product.id) {
+    //         dispatch(addProductToWishList(product.id));
+    //     }
+    // };
+
+    // Todo: Return fallback error component
+    if (product === null || product === undefined) {
+        return null;
+    }
+
+    const { availableColors, availableSizes, productVariantProperty } =
+        getProductVariantProperties(product);
+
+    const [selectedColor, setSelectedColor] = React.useState<string>(
+        selectedProductVariant.properties.find(
+            (property) => property.name.toLowerCase() === productVariantProperty.COLOR
+        )?.value ?? ""
     );
+    const [selectedSize, setSelectedSize] = React.useState<string>("");
 
-    const handleAddToCart = () => {
-        // Handle already exists condition as well with alert message.
-        const productWithStrictType = product as Product;
-        dispatch(addCartItem(productWithStrictType, 1));
+    const handleColorSelect = (color: string) => {
+        // Whenever a new color is selected, make sure the size is also reset.
+        setSelectedColor(color);
+        setSelectedSize("");
+
+        // Search for the productVariant with same color and set it as selected
+        const productVariant = product.productVariants.find((productVariant) =>
+            productVariant.properties.some(
+                (property) =>
+                    property.name === productVariantProperty.COLOR && property.value === color
+            )
+        );
+        if (productVariant) {
+            setSelectedProductVariant(productVariant);
+        }
     };
 
-    const handleAddToWishList = () => {
-        if (product.id) {
-            dispatch(addProductToWishList(product.id));
+    const handleSizeSelect = (size: string) => {
+        setSelectedSize(size);
+
+        // Search for the productVariant with same color and size and set it as selected
+        const productVariant = product.productVariants.find(
+            (productVariant) =>
+                productVariant.properties.some(
+                    (property) =>
+                        property.name === productVariantProperty.COLOR &&
+                        property.value === selectedColor
+                ) &&
+                productVariant.properties.some(
+                    (property) =>
+                        property.name === productVariantProperty.SIZE && property.value === size
+                )
+        );
+        if (productVariant) {
+            setSelectedProductVariant(productVariant);
         }
     };
 
     return (
-        <div>
-            <div className="text-4xl">{product.brand}</div>
-            <div className="text-2xl py-2 text-default-500">{product.name}</div>
-            <Divider />
+        <div className="mt-3 space-y-3">
+            <div className="text-3xl uppercase font-semibold">{product.brand}</div>
+            <div className="text-2xl text-default-500 capitalize">{product.name}</div>
             <Rating rating={product.rating || 0} reviewCount={product.reviewCount || 0} />
-            <Divider />
-            <div className="flex py-2 text-2xl xs:text-lg">
-                <div className="flex font-semibold">
-                    <RupeeIcon height={22} width={22} size={22} className="my-1" />{" "}
-                    {product.sellingPrice}
+            <div className="flex items-center text-2xl xs:text-lg">
+                <div className="flex items-center font-semibold">
+                    <RupeeIcon height={22} width={22} size={22} className="" />{" "}
+                    {selectedProductVariant.sellingPrice}
                 </div>
-                <div className="flex pl-3 line-through text-default-500 font-semibold">
-                    <RupeeIcon height={22} width={22} size={22} className="my-1" /> {product.price}
+                <div className="flex pl-3 items-center line-through text-default-500 font-semibold">
+                    <RupeeIcon height={22} width={22} size={22} className="" />{" "}
+                    {selectedProductVariant.price}
                 </div>
-                <div className="pl-3 text-green-600 font-medium">({product.discount}% Off)</div>
-            </div>
-            <Divider />
-            <div className="flex py-3 text-xl">
-                <div>Availability:</div>
-                <Spacer />
-                <div
-                    className={
-                        product.stockCount !== undefined && product.stockCount <= 0
-                            ? "text-red-500 font-semibold"
-                            : "text-green-600 font-semibold"
-                    }
-                >
-                    {product.stockCount !== undefined && product.stockCount > 0
-                        ? "In Stock"
-                        : "Out of Stock"}
+                <div className="pl-3 text-orange-600 font-medium">
+                    ({selectedProductVariant.discount}% Off)
                 </div>
             </div>
-            <Divider />
-            <div className="py-5 flex">
-                <Button
-                    fullWidth
-                    size="lg"
-                    variant="ghost"
-                    color="warning"
-                    onPress={handleAddToCart}
-                    isDisabled={isProductInCart}
-                >
-                    Add to Cart
-                </Button>
-                <Spacer x={7} />
-                <Button
-                    fullWidth
-                    size="lg"
-                    variant="ghost"
-                    color="danger"
-                    onPress={handleAddToWishList}
-                    isDisabled={isProductInWishList}
-                >
-                    Add to Wishlist
-                </Button>
-            </div>
-            <Divider />
-
-            <div>
-                {product.stockCount && product.stockCount > 0 && (
-                    <div>
-                        <div className="flex py-3 text-xl font-medium">
-                            Delivery Options{" "}
-                            <TruckIcon
-                                width={24}
-                                height={24}
-                                size={24}
-                                strokeWidth={2}
-                                className="mx-3 my-1"
-                            />
-                        </div>
-                        <div>
-                            <div className="flex py-1">
-                                <TruckFastIcon width={24} height={24} size={24} className="mr-3" />
-                                Get it by {getDeliveryDate()}
-                            </div>
-                            <div className="flex py-1">
-                                <CashIcon width={28} height={28} size={24} className="mr-3" />
-                                Pay on Delivery available
-                            </div>
-                            <div className="flex py-1">
-                                <ExchangeIcon width={28} height={28} size={24} className="mr-3" />
-                                Easy 14 days return & exchange available
-                            </div>
-                        </div>
+            <span className="text-green-600">inclusive of all taxes</span>
+            {availableColors.length && (
+                <div className="space-y-4">
+                    <div className="capitalize text-lg font-semibold">Available Colors</div>
+                    <div className="flex flex-wrap gap-4 justify-start items-center">
+                        {availableColors.map((color, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    data-selected={color.name === selectedColor}
+                                    onClick={() => handleColorSelect(color.name)}
+                                    className="w-24 cursor-pointer rounded-medium ring-offset-background transition-shadow data-[selected=true]:outline-none data-[selected=true]:ring-2 data-[selected=true]:ring-focus data-[selected=true]:ring-offset-2"
+                                >
+                                    <Image
+                                        src={color.image ?? PLACEHOLDER_IMAGE}
+                                        alt={product.name}
+                                    />
+                                </div>
+                            );
+                        })}
                     </div>
-                )}
+                </div>
+            )}
+            <Spacer y={2} />
+            {availableSizes.length && (
+                <div className="space-y-2">
+                    <div className="capitalize text-lg font-semibold">Available Sizes</div>
+                    {!selectedSize && (
+                        <span className="text-rose-500 text-sm">Please select a size</span>
+                    )}
+                    <div className="flex flex-wrap gap-4">
+                        {availableSizes.map((size) => (
+                            <Button
+                                key={size}
+                                isIconOnly
+                                radius="full"
+                                color="primary"
+                                variant={size === selectedSize ? "solid" : "ghost"}
+                                className="capitalize"
+                                isDisabled={
+                                    !selectedColor ||
+                                    !product.productVariants.some(
+                                        (productVariant) =>
+                                            productVariant.properties.find(
+                                                (property) =>
+                                                    property.name === productVariantProperty.COLOR
+                                            )?.value === selectedColor &&
+                                            productVariant.properties.find(
+                                                (property) =>
+                                                    property.name === productVariantProperty.SIZE
+                                            )?.value === size
+                                    )
+                                }
+                                onClick={() => handleSizeSelect(size)}
+                            >
+                                {size}
+                            </Button>
+                        ))}
+                    </div>
+                </div>
+            )}
+            <Spacer y={2} />
+            <div className="flex gap-3">
+                <Button
+                    fullWidth
+                    size="lg"
+                    color="warning"
+                    variant="ghost"
+                    startContent={<CartIcon width={22} height={22} size={22} />}
+                    isDisabled={!selectedColor || !selectedSize}
+                >
+                    Add to cart
+                </Button>
+                <Button
+                    fullWidth
+                    size="lg"
+                    color="danger"
+                    variant="ghost"
+                    startContent={<HeartIcon width={24} height={24} size={24} />}
+                    isDisabled={!selectedColor || !selectedSize}
+                >
+                    Add to wishlist
+                </Button>
             </div>
-            <Divider className="my-2" />
-            <div className="py-3">
-                <div className="text-xl">Product Details</div>
-                <div className="py-3 text-default-500 text-lg">{product.description}</div>
-            </div>
+            <div className="text-default-500 text-lg">{product.description}</div>
         </div>
     );
 }
--- src/components/ProductScreen/ProductImages.tsx
+++ src/components/ProductScreen/ProductImages.tsx
@@ -1,58 +1,44 @@
-import { Button, Image, ScrollShadow, Skeleton } from "@nextui-org/react";
+import { Button, Image, ScrollShadow } from "@nextui-org/react";
+import { Product, ProductVariant } from "utils/types";
 
 import { PLACEHOLDER_IMAGE } from "constants/images";
-import { Product } from "utils/types";
 import React from "react";
 
 interface ProductImagesProps {
-    product: Partial<Product>;
-    isLoading?: boolean;
+    product: Product;
+    selectedProductVariant: ProductVariant;
 }
 
-function ProductImages({ product, isLoading }: ProductImagesProps) {
-    const featuredImage = product.productImages?.find(
-        (productImage) => productImage.isFeatured === true
-    );
+function ProductImages({ product, selectedProductVariant }: ProductImagesProps) {
+    if (product === null || product === undefined) {
+        return null;
+    }
 
-    const [selectedImage, setSelectedImage] = React.useState(featuredImage?.image);
+    const [selectedImage, setSelectedImage] = React.useState(selectedProductVariant?.images[0]);
 
-    // Adding useEffect because `selectedImage` will be initially set to `undefined` due to `find` function
-    // used on productImages.
     React.useEffect(() => {
-        if (featuredImage && featuredImage.image) {
-            setSelectedImage(featuredImage.image);
-        }
-    }, [featuredImage]);
+        setSelectedImage(selectedProductVariant.images[0]);
+    }, [selectedProductVariant]);
 
     return (
         <div>
-            {isLoading ? (
-                <Skeleton className="w-11/12 rounded-lg mb-5">
-                    <div className="h-96 bg-default-300"></div>
-                </Skeleton>
-            ) : (
-                <div className="w-5/6">
-                    <Image
-                        src={selectedImage || featuredImage?.image || PLACEHOLDER_IMAGE}
-                        alt={product.name}
-                        isBlurred
-                    />
-                    <ScrollShadow hideScrollBar orientation="horizontal" className="flex gap-4 p-4">
-                        {product.productImages?.map((productImage) => {
-                            return (
-                                <Button
-                                    key={productImage.id}
-                                    data-selected={productImage.image === selectedImage}
-                                    className="h-24 w-24 bg-gray-200 flex-none cursor-pointer items-center justify-center rounded-medium ring-offset-background transition-shadow data-[selected=true]:outline-none data-[selected=true]:ring-2 data-[selected=true]:ring-focus data-[selected=true]:ring-offset-2"
-                                    onClick={() => setSelectedImage(productImage.image)}
-                                >
-                                    <Image src={productImage.image} alt="productImage"/>
-                                </Button>
-                            );
-                        })}
-                    </ScrollShadow>
-                </div>
-            )}
+            <div className="w-5/6">
+                <Image src={selectedImage || PLACEHOLDER_IMAGE} alt={product.name} isBlurred />
+                <ScrollShadow hideScrollBar orientation="horizontal" className="flex gap-4 p-4">
+                    {selectedProductVariant?.images.map((productImage) => {
+                        return (
+                            <Button
+                                key={productImage}
+                                data-selected={productImage === selectedImage}
+                                className="h-24 w-24 bg-gray-200 flex-none cursor-pointer items-center justify-center rounded-medium ring-offset-background transition-shadow data-[selected=true]:outline-none data-[selected=true]:ring-2 data-[selected=true]:ring-focus data-[selected=true]:ring-offset-2"
+                                onClick={() => setSelectedImage(productImage)}
+                            >
+                                <Image src={productImage} alt="productImage" />
+                            </Button>
+                        );
+                    })}
+                </ScrollShadow>
+            </div>
         </div>
     );
 }
