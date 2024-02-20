77de182269d55e5e25484ca2de93be3ce4b5e54c
Srivatsa J <srivatsa1199@gmail.com>
2024-02-21 01:06:58 +0530

    Fix Product screen

--- src/screens/ProductService/ProductScreen.tsx
+++ src/screens/ProductService/ProductScreen.tsx
@@ -1,27 +1,32 @@
 import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
 import { CATEGORY_SCREEN, HOME_SCREEN } from "constants/routes";
-import { useReduxDispatch, useReduxSelector } from "hooks/redux";
 
 import ProductDetails from "components/ProductScreen/ProductDetails";
 import ProductDetailsSkeleton from "components/ProductScreen/ProductDetailsSkeleton";
 import ProductImages from "components/ProductScreen/ProductImages";
+import ProductImagesSkeleton from "components/ProductScreen/ProductImagesSkeleton";
+import { ProductVariant } from "utils/types";
 import React from "react";
-import { getProductDetails } from "redux/ProductService/productByIdSlice";
 import { useParams } from "react-router-dom";
+import { useReduxSelector } from "hooks/redux";
 
 function ProductScreen() {
     const { id } = useParams();
+
+    // Todo: Return fallback error component instead of null
     if (id === undefined) {
         return null;
     }
-    const typeCastedId = BigInt(id);
-    const dispatch = useReduxDispatch();
-    const productDetails = useReduxSelector((state) => state.productDetails);
-    const { product, isLoading } = productDetails;
 
-    React.useEffect(() => {
-        dispatch(getProductDetails(typeCastedId));
-    }, [dispatch, typeCastedId]);
+    const { products, isLoading } = useReduxSelector((state) => state.productList);
+    const product = products.find((product) => Number(product.id) === Number(id));
+
+    // Todo: Return fallback error component instead of null
+    if (!products || !products.length || !product) return null;
+
+    const [selectedProductVariant, setSelectedProductVariant] = React.useState<ProductVariant>(
+        product.productVariants[0]
+    );
 
     return (
         <div className="container mx-auto px-6 py-7">
@@ -39,10 +44,25 @@ function ProductScreen() {
             </Breadcrumbs>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4">
                 <div>
-                    <ProductImages product={product} isLoading={isLoading} />
+                    {isLoading ? (
+                        <ProductImagesSkeleton />
+                    ) : (
+                        <ProductImages
+                            product={product}
+                            selectedProductVariant={selectedProductVariant}
+                        />
+                    )}
                 </div>
                 <div>
-                    {isLoading ? <ProductDetailsSkeleton /> : <ProductDetails product={product} />}
+                    {isLoading ? (
+                        <ProductDetailsSkeleton />
+                    ) : (
+                        <ProductDetails
+                            product={product}
+                            selectedProductVariant={selectedProductVariant}
+                            setSelectedProductVariant={setSelectedProductVariant}
+                        />
+                    )}
                 </div>
             </div>
         </div>
