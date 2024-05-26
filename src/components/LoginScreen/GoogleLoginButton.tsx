35a91a37868cefc1d574ce4d454d2f55c15ad6ed
Srivatsa J <srivatsa1199@gmail.com>
2024-05-27 02:10:36 +0530

    Update env variables

--- src/components/LoginScreen/GoogleLoginButton.tsx
+++ src/components/LoginScreen/GoogleLoginButton.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@nextui-org/react";
-import { GOOGLE_LOGIN_USER_SCREEN } from "constants/routes";
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 
@@ -14,13 +13,13 @@ function GoogleLoginButton() {
             "https://www.googleapis.com/auth/userinfo.profile"
         ].join(" ");
 
-        if (process.env.REACT_APP_GOOGLE_CLIENT_ID && process.env.REACT_APP_BASE_FRONTEND_URL) {
+        if (
+            process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID &&
+            process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL
+        ) {
             queryParams.set("response_type", "code");
-            queryParams.set("client_id", process.env.REACT_APP_GOOGLE_CLIENT_ID);
-            queryParams.set(
-                "redirect_uri",
-                `${process.env.REACT_APP_BASE_FRONTEND_URL}${GOOGLE_LOGIN_USER_SCREEN}`
-            );
+            queryParams.set("client_id", process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID);
+            queryParams.set("redirect_uri", process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL);
             queryParams.set("prompt", "select_account");
             queryParams.set("scope", scope);
             setQueryParams(queryParams);
